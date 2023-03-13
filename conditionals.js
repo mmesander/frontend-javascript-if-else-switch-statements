1877d7f3ebe36ce87805750c41353ecd9c9c2b5e
mmesander <mark_mesander@hotmail.com>
2023-03-13 17:04:52 +0100

    Opdracht 1 t/m 4 van conditionals gemaakt

--- conditionals.js
+++ conditionals.js
@@ -9,16 +9,22 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
 
+const biggerThen = 10 > 20;
+console.log('Is 10 groter dan 20', biggerThen);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
 
+const sameNumber = 10 !== 11;
+console.log('Is 10 ongelijk aan 11', sameNumber);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
 
+const sameWord = 'zoet' === 'sappig';
+console.log('is zoet en sappig hetzelfde', sameWord)
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
@@ -26,6 +32,10 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
 
+const grade = 9;
+if (grade > 8) {
+    console.log("Cum Laude!")
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
